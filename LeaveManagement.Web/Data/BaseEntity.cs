cced8d743045f303375177550e7ac461dbe21b5a
yanyanRyan <rpecarma@gmail.com>
2021-12-14 14:30:11 -0600

    Added Database Tables and Migrations.

---
+++ LeaveManagement.Web/Data/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagement.Web.Data
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        // can prepend LeaveType to Id and EF knows its a primary,  or in this case "Id" by itself EF knows its a primary key
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
---
+++ LeaveManagement.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagement.Web.Data
+{
+    public class LeaveAllocation : BaseEntity
+    {
+        
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")] // can also use data annotation
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        //if you prepend the Type with Id, EF will know LeaveTypeId is a foreign key referencing LeaveType
+
+        public string EmployeeId { get; set; }
+
+        
+
+    }
+}
