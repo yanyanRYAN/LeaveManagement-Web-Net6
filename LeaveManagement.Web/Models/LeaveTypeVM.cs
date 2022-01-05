920e1bdc1eccbcb0b65d6d88b7fe603f31240037
yanyanRyan <rpecarma@gmail.com>
2022-01-05 16:28:47 -0600

    Modified the crud screens, added datatables, swal, font awesome for Leave Type Management

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    //view model for LeaveType
+    public class LeaveTypeVM
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Leave Type Name")]
+        [Required]
+        public string Name { get; set; }
+
+        [Display(Name = "Default Number Of Days")]
+        [Required]
+        [Range(1, 25, ErrorMessage = "Please Enter A Valid Number")]
+        public int DefaultDays { get; set; }
+
+    }
+}
